511dd96b7ab7b5cdebf156ed1dd0f6fbdeed0917
Virat Singh <114825043+viratsingh24@users.noreply.github.com>
2024-01-17 16:55:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#print("hello world")#hey virat how are uh
+#comments in python
+'''print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")'''
+#comments used to describe the things
+#variables in python(variables are used to store anything)
+a=3
+print(a)
+b=55
+print(b)
+a=6.2
+print(a)
+c= True# false they are boolean data types
+print(c)
+d="harry"
+print(d)
+e=None#also a data types
+print(e)
