32fb6e7c2963229382743527843ee2c20c46fdee
Virat Singh <114825043+viratsingh24@users.noreply.github.com>
2024-01-17 16:55:40 +0530

    Add files via upload

---
+++ strings deepdive.py
@@ -0,0 +1,5 @@
+name= "virat"
+print(name)
+print(name[0:3])#start from 0 and go all the way till 2
+print(name[0:4])#start from 0 and go all the way till (4-1)
+#print(name[a:b])#start from a and go all the way till b-1
